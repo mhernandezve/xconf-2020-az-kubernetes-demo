bc1b434939853dc761ad0e0f5e5bf6d737faa310
Miguel Hernández Giusti <mhernandezve@users.noreply.github.com>
2020-10-20 20:21:13 -0500

    Create variables.tf

---
+++ demo/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = "string"
+  description = "Name of the azure resource group."
+}
+
+variable "resource_group_location" {
+  type        = "string"
+  description = "Location of the azure resource group."
+}
